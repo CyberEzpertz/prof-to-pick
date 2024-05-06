2e1a0726500df1c3907808f8e4fd0435d8aa1643
CyberEzpertz <skyblaze24@gmail.com>
2024-05-06 15:26:05 +0800

    test: add console log to check for profs

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -12,6 +12,10 @@ export default async function Home() {
     getCachedRecents(),
   ]);
 
+  console.log(courses);
+  console.log(profs);
+  console.log(recents);
+
   return (
     <div className="flex h-full w-full min-w-0 grow-0 flex-col items-center justify-center gap-4 bg-gradient bg-cover px-5 lg:px-10">
       <SearchBar recents={recents} courses={courses} profs={profs} />
